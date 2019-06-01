15eb13524c80d0e430ea1f4292bfd9d48b48a736
Faryyaz <dilfary@hotmail.com>
2019-06-01 09:09:22 +0400

    Login page styling

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
+import Vuetify from 'vuetify';
 
 Vue.config.productionTip = false
+Vue.use(Vuetify);
 
 new Vue({
   router,
