2e8debfecd99ffb069f4958abe4d54d4a03c2f8a
Faryyaz <dilfary@hotmail.com>
2019-06-04 06:06:38 +0400

    Added transition when changing pages

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <v-app>
     <v-content>
+      <transition name="fade" mode="out-in">
       <router-view></router-view>
+    </transition>
     </v-content>
   </v-app>
 </template>
@@ -17,3 +19,16 @@ export default {
   }
 }
 </script>
+<style>
+  .fade-enter-active,
+  .fade-leave-active {
+    transition-duration: 0.3s;
+    transition-property: opacity;
+    transition-timing-function: ease;
+  }
+
+  .fade-enter,
+  .fade-leave-active {
+    opacity: 0
+  }
+</style>
