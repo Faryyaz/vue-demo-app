eb4b925613a0fbabfe2943e34144f98dd3e4c252
Faryyaz <dilfary@hotmail.com>
2019-06-04 09:06:13 +0400

    Added services + router guard (WIP)

---
+++ src/_services/auth-header.service.ts
@@ -0,0 +1,15 @@
+export class AuthHeaderService {
+    user: any;
+
+    public init() {
+        // return authorization header with basic auth credentials
+        this.user = JSON.parse(localStorage.getItem('user'));
+
+        if (this.user && this.user.authdata) {
+            return { 'Authorization': 'Basic ' + this.user.authdata };
+        } else {
+            return {};
+        }
+    }
+
+}
--- src/router.ts
+++ src/router.ts
@@ -8,7 +8,7 @@ import Dashboard from './views/dashboard/index.vue';
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -55,3 +55,21 @@ export default new Router({
     // }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  // redirect to login page if not logged in and trying to access a restricted page
+  const publicPages = ['/login'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('user');
+
+  if (authRequired && !loggedIn) {
+    return next({ 
+      path: '/login', 
+      query: { returnUrl: to.path } 
+    });
+  }
+
+  next();
+})
+
+export default { router }
