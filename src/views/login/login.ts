4fab12fee5c533d8b7c0bc667b8cea264c9fe679
Faryyaz <dilfary@hotmail.com>
2019-06-06 00:24:12 +0400

    Login transition (WIP)

--- src/views/login/login.ts
+++ src/views/login/login.ts
@@ -1,45 +1,16 @@
 import { Component, Vue } from 'vue-property-decorator';
-import UserService from '@/_services/user.service';
-import router from '@/router';
+import Form from './form/index.vue';
+import Loader from './loader/index.vue';
+import Error from './error/index.vue';
 
 @Component({
-  
+  components: {
+    Form,
+    Loader,
+    Error
+  }
 })
 export default class Login extends Vue {
-  userService: any;
-  returnUrl: any;
-  merchantID: string = "";
-  password: string = "";
-  submitted: boolean = false;
-  
-  mounted () {
-    this.userService = new UserService();
-    this.merchantID = '';
-    this.password = '';
-
-    // reset login status
-    this.userService.logout();
-
-    // get return url from route parameters or default to '/'
-    this.returnUrl = this.$route.query.returnUrl || "/";
-  }
-
-
-  handleSubmit() {
-    this.submitted = true;
-    const { merchantID, password } = this;
-
-    // stop here if form is invalid
-    if (!(merchantID && password)) {
-      return;
-    }
-
-    // this.loading = true;
-    this.userService.login(merchantID, password).then(
-      (user: any) => router.push(this.returnUrl),
-      (error: Error) => console.log(error)
-    );
-  }
-
+  view: any = Form;
 
 }
