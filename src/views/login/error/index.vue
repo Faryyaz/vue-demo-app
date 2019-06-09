cbcc3be714aca6e4abd85252c56b2b2cb31e0941
Faryyaz <dilfary@hotmail.com>
2019-06-09 10:04:51 +0400

    Naming conflict, view/component deleted

--- src/views/login/error/index.vue
+++
@@ -1,4 +0,0 @@
-<template src="./error.html"></template>
-<script src="./error.ts" lang="ts"></script>
-<style src="./error.scss" scoped lang="scss"></style>
-
--- src/views/login/form/index.vue
+++
@@ -1,4 +0,0 @@
-<template src="./form.html"></template>
-<script src="./form.ts" lang="ts"></script>
-<style src="./form.scss" scoped lang="scss"></style>
-
