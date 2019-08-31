2f2f6207fdd578e5f3332f1697cac358d33bdcee
Faryyaz <dilfary@hotmail.com>
2019-08-31 16:16:53 +0400

    Added form demo to add values

--- src/views/dashboard/dashboard.ts
+++ src/views/dashboard/dashboard.ts
@@ -5,5 +5,25 @@ import { Component, Vue } from 'vue-property-decorator';
   },
 })
 export default class Layout extends Vue {
+  weight: number | null = null;
+  amount: number | null = null;
+  collection: Array<Object> = [];
+
+  add() {
+    let bracket = {
+      weight: this.weight,
+      amount: this.amount
+    }
+
+    this.collection.push(bracket);
+
+    this.weight = null;
+    this.amount = null;
+  }
+
+  remove(index: number) {
+    this.collection.splice(index, 1);
+  }
+
 
 }
